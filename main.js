4892732c70dbe2d139ee85996e848190bc07c65e
Naggelus <foreyoue@gmail.com>
2019-04-01 14:46:53 +0200

    Event-handler added

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 const { Client } = require('discord.js');
+const fs = require('fs');
+
 const WS = require('./ws/ws');
 
 const config = require('./config.json');
-
 var client = new Client();
 client.config = config;
 
 var ws = new WS(config.ws.port, client);
 
-client.on('ready', () => {
-  console.log(`Connected as ${client.user.tag}`);
+fs.readdir('./events/', (err, files) => {
+  if (err) return console.log(err);
+  files.forEach(file => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split('.')[0];
+    client.on(eventName, event.bind(null, client));
+  });
 });
 
 client.login(config.token || process.env.token);
