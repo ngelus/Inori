fe1d2c83fa7fc385642ece5cf59399cf7e3de6fe
Naggelus <foreyoue@gmail.com>
2019-04-02 10:46:31 +0200

    extended on help-command

--- commands/Information/help.js
+++ commands/Information/help.js
@@ -1,8 +1,12 @@
 exports.description = 'Shows this help';
 
+const { RichEmbed } = require('discord.js');
 exports.run = (client, message, args) => {
   message.delete();
 
   var helpstring = '';
-  
+  const embed = new RichEmbed()
+    .setTitle('Help')
+    .setAuthor(client.user.username, client.user.avatarURL)
+    .setColor(0x00ae86);
 };
